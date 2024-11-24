12824f53708e46933932baaf200d9c83319aba9c
Sathish A <sathish97a@gmail.com>
2024-11-24 21:46:46 +0530

    Update transform.py
    fix np.math

--- fdlite/transform.py
+++ fdlite/transform.py
@@ -189,7 +189,7 @@ def bbox_to_roi(
     """
     if not bbox.normalized:
         raise CoordinateRangeError('bbox must be normalized')
-    PI = np.math.pi
+    PI = np.pi
     TWO_PI = 2 * PI
     # select ROI dimensions
     width, height = _select_roi_size(bbox, image_size, size_mode)
@@ -202,9 +202,9 @@ def bbox_to_roi(
         return Rect(cx, cy, width, height, rotation=0., normalized=True)
     x0, y0 = rotation_keypoints[0]
     x1, y1 = rotation_keypoints[1]
-    angle = -np.math.atan2(y0 - y1, x1 - x0)
+    angle = -np.atan2(y0 - y1, x1 - x0)
     # normalise to [0, 2*PI]
-    rotation = angle - TWO_PI * np.math.floor((angle + PI) / TWO_PI)
+    rotation = angle - TWO_PI * np.floor((angle + PI) / TWO_PI)
     return Rect(cx, cy, width, height, rotation, normalized=True)
 
 
@@ -291,7 +291,7 @@ def project_landmarks(
         return [Landmark(x, y, z) for (x, y, z) in points]
     # coordinate system transformation from ROI- to image space
     norm_roi = roi.scaled(image_size, normalize=True)
-    sin, cos = np.math.sin(roi.rotation), np.math.cos(roi.rotation)
+    sin, cos = np.sin(roi.rotation), np.cos(roi.rotation)
     matrix = np.array([[cos, sin, 0.], [-sin, cos, 0.], [1., 1., 1.]])
     points -= (0.5, 0.5, 0.0)
     rotated = np.matmul(points * (1, 1, 0), matrix)
